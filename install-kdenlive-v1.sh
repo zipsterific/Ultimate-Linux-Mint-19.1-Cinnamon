e24b52d9412e23fd52064f3d4132d059cd21144b
Big Al <fatbikeguy@protonmail.com>
2019-02-04 15:48:35 -0600

    various updates

--- install-kdenlive-v1.sh
+++ install-kdenlive-v1.sh
@@ -11,7 +11,7 @@
 #
 ##################################################################################################################
 
-wget https://files.kde.org/kdenlive/release/kdenlive-18.12.1b-x86_64.appimage -O /home/$USER/
+wget https://files.kde.org/kdenlive/release/kdenlive-18.12.1b-x86_64.appimage -O $USER/apps
 
 echo "################################################################"
 echo "#############   stand-alone kdenlive installed   ###############"
