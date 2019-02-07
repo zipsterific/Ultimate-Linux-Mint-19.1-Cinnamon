fa76809ae707440f5c3e6297b6b5bed797a4751f
Al <fatbikeguy@protonmail.com>
2019-02-06 20:58:20 -0600

    added restic

--- 1-install-core-software-v2.sh
+++ 1-install-core-software-v2.sh
@@ -19,7 +19,7 @@ fi
 
 #software from 'normal' repositories
 apt-get install -y catfish clementine curl dconf-cli dconf-editor dropbox evolution focuswriter geany geary gimp gpick
-apt-get install -y glances gparted grsync hardinfo inkscape meld kdenlive frei0r-plugins nfs-common autofs
+apt-get install -y glances gparted grsync hardinfo inkscape meld kdenlive frei0r-plugins nfs-common autofs restic
 apt-get install -y openshot pinta plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse
 apt-get install -y thunar vlc vnstat   
 
