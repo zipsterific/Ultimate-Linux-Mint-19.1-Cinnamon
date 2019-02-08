adbeae6bf609424d071c48f582c305170feef6d4
Al <fatbikeguy@protonmail.com>
2019-02-07 22:27:08 -0600

    cpu scaling utils added

--- 1-install-core-software-v2.sh
+++ 1-install-core-software-v2.sh
@@ -21,7 +21,8 @@ fi
 apt-get install -y catfish clementine curl dconf-cli dconf-editor dropbox evolution focuswriter geany geary gimp gpick
 apt-get install -y glances gparted grsync hardinfo inkscape meld kdenlive frei0r-plugins nfs-common autofs restic
 apt-get install -y openshot pinta plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse
-apt-get install -y thunar vlc vnstat ubuntu-restricted-addons ubuntu-restricted-extras
+apt-get install -y thunar vlc vnstat ubuntu-restricted-addons ubuntu-restricted-extras powertop linux-tools-common
+apt-get install -y linux-tools-generic linux-cloud-tools-generic
 
 # Removals
 apt-get remove -y xplayer
