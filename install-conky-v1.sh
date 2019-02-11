dadd6cd85efa6429a989081b10f7a60125775923
Al <fatbikeguy@protonmail.com>
2019-02-10 19:24:56 -0600

    Update install-conky-v1.sh

--- install-conky-v1.sh
+++ install-conky-v1.sh
@@ -1,3 +1,25 @@
 sudo add-apt-repository -y ppa:teejee2008/ppa
 sudo apt-get update
 sudo apt-get install conky conky-manager
+
+
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo add-apt-repository -y ppa:teejee2008/ppa
+sudo apt-get update
+sudo apt-get install conky-all conky-manager
+
+echo "################################################################"
+echo "######################## conky installed #######################"
+echo "################################################################"
