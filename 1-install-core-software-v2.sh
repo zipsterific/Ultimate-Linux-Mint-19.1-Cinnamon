5773f1e2cc5a807de277f987978213e8bee03b78
Al <fatbikeguy@protonmail.com>
2019-02-10 19:14:38 -0600

    python-gpg

--- 1-install-core-software-v2.sh
+++ 1-install-core-software-v2.sh
@@ -22,7 +22,7 @@ apt-get update -y
 apt-get install -y catfish clementine curl dconf-cli dconf-editor dropbox evolution focuswriter geany geary gimp gpick
 apt-get install -y glances gparted grsync hardinfo inkscape meld kdenlive frei0r-plugins nfs-common autofs restic screen
 apt-get install -y openshot pinta plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse photocollage
-apt-get install -y thunar vlc vnstat ubuntu-restricted-addons ubuntu-restricted-extras powertop linux-tools-common
+apt-get install -y thunar vlc vnstat ubuntu-restricted-addons ubuntu-restricted-extras powertop linux-tools-common python-gpg
 apt-get install -y linux-tools-generic linux-cloud-tools-generic conky-all apcupsd audacious moc mpd xmms2 openssh-server
 apt-get upgrade -y
 
